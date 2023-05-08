361b4e08971c2cad8ef49f7ccc7ff06611f3d381
Tyler <tylercaillet2121@gmail.com>
2023-05-08 15:48:21 -0400

    controllers, middleware, and routes created

---
+++ controllers/AuthController.js
@@ -0,0 +1,47 @@
+const { User } = require('../models')
+const middleware = require('../middleware')
+
+const Login = async (req, res) => {
+  try {
+    const user = await User.findOne({
+      where: { username: req.body.username },
+      raw: true
+    })
+    if (
+      user &&
+      (await middleware.comparePassword(user.passwordDigest, req.body.password))
+    ) {
+      let payload = {
+        id: user.id,
+        username: user.username
+      }
+      let token = middleware.createToken(payload)
+      return res.send({ user: payload, token })
+    }
+    res.status(401).send({ status: 'Error', msg: 'Unauthorized' })
+  } catch (error) {
+    throw error
+  }
+}
+
+const Register = async (req, res) => {
+  try {
+    const { username, password } = req.body
+    let passwordDigest = await middleware.hashPassword(password)
+    const user = await User.create({ username, passwordDigest })
+    res.send(user)
+  } catch (error) {
+    throw error
+  }
+}
+
+const CheckSession = async (req, res) => {
+  const { payload } = res.locals
+  res.send(payload)
+}
+
+module.exports = {
+  Login,
+  Register,
+  CheckSession
+}
--- seeders/20230508174611-review.js
+++ seeders/20230508174611-review.js
@@ -4,7 +4,7 @@ const falso = require('@ngneat/falso')
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up(queryInterface, Sequelize) {
-    const comments = [...Array(5)].map((_) => {
+    const reviews = [...Array(5)].map((_) => {
       return {
         userId: falso.randNumber({ min: 1, max: 20 }),
         listingId: falso.randNumber({ min: 1, max: 20 }),
