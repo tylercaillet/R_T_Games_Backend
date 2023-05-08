f8e4d7ee30214d0eda6781c69cb23195fd8db0d0
Tyler <tylercaillet2121@gmail.com>
2023-05-08 12:27:51 -0400

    migrations, model, and seed file for user

---
+++ models/user.js
@@ -0,0 +1,45 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class User extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+      User.hasMany(models.Review, {
+        foreign: "userId",
+        as: "user_review",
+        onDelete: 'CASCADE',
+        onUpdate: 'CASCADE'
+      })
+      // User.belongsToMany(models.Review, {
+      //   as: "",
+      //   // through:models.Games,
+      //   foreignKey: "id"
+      // })
+    }
+  }
+  User.init(
+    {
+    username: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    passwordDigest: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+  }, {
+    sequelize,
+    modelName: 'User',
+    tableName: 'users'
+    }
+  );
+  
+  return User;
+};
---
+++ seeders/20230508162533-user.js
@@ -0,0 +1,21 @@
+'use strict'
+const falso = require('@ngneat/falso')
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    const users = [...Array(20)].map((_) => {
+      return {
+        userName: falso.randUserName(),
+        passwordDigest: falso.randPassword(),
+        createdAt: new Date(),
+        updatedAt: new Date()
+      }
+    })
+    await queryInterface.bulkInsert('users', users)
+  },
+
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.bulkDelete('users', null, {})
+  }
+}
