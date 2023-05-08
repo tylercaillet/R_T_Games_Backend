26e79231cacaabbb3c69ee54f4891b31b0cc4d11
Tyler <tylercaillet2121@gmail.com>
2023-05-08 15:39:16 -0400

    review seeder updated

--- seeders/20230508174611-review.js
+++ seeders/20230508174611-review.js
@@ -1,25 +1,23 @@
-'use strict';
+'use strict'
+const falso = require('@ngneat/falso')
 
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
-  async up (queryInterface, Sequelize) {
-    /**
-     * Add seed commands here.
-     *
-     * Example:
-     * await queryInterface.bulkInsert('People', [{
-     *   name: 'John Doe',
-     *   isBetaMember: false
-     * }], {});
-    */
+  async up(queryInterface, Sequelize) {
+    const comments = [...Array(5)].map((_) => {
+      return {
+        userId: falso.randNumber({ min: 1, max: 20 }),
+        listingId: falso.randNumber({ min: 1, max: 20 }),
+        review: falso.randPhrase(),
+        createdAt: new Date(),
+        updatedAt: new Date()
+      }
+    })
+    await queryInterface.bulkInsert('reviews', reviews)
   },
 
-  async down (queryInterface, Sequelize) {
-    /**
-     * Add commands to revert seed here.
-     *
-     * Example:
-     * await queryInterface.bulkDelete('People', null, {});
-     */
+  async down(queryInterface, Sequelize) {
+    await queryInterface.bulkDelete('reviews')
   }
-};
+}
+
